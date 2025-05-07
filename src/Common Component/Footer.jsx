b9a00a3486f638853619db82287687dd6a748aa3
NurTheDev <nur756.islam@gmail.com>
2025-05-08 00:58:12 +0600

    added fotter page

--- src/Common Component/Footer.jsx
+++ src/Common Component/Footer.jsx
@@ -19,29 +19,30 @@ const socialIcons = [{
 
 function Footer() {
     return (
-        <div className={"bg-[rgba(255,220,38,0.50)] -mt-40 py-10"}>
+        <div className={"bg-[rgba(255,220,38,0.50)] -mt-52 lg:mt-40 py-10 px-4 lg:px-0"}>
             <div className={"container mx-auto"}>
-                <div className={"flex justify-between items-center mt-72"}>
-                    <div className={"flex flex-col space-y-5 w-1/3"}>
+                <div className={"flex flex-col lg:flex-row justify-between lg:items-center mt-72"}>
+                    <div className={"flex flex-col space-y-5 lg:w-1/3"}>
                         <div className={"flex items-center space-x-3 "}>
                             <img src={logoBlack || ""} alt="logoBlack"/>
-                            <p className={"font-Sora text-[26px]"}>Browse<span className={"text-accent"}>4</span>Pets
+                            <p className={"font-Sora text-xl lg:text-[26px]"}>Browse<span
+                                className={"text-accent"}>4</span>Pets
                             </p>
                         </div>
-                        <p className={"text-xl"}>Discover a world of treats, toys, and essentials handpicked for
+                        <p className={"lg:text-xl"}>Discover a world of treats, toys, and essentials handpicked for
                             your
                             furry
                             friends</p>
                     </div>
 
-                    <ul className={"flex flex-col space-y-3"}>
+                    <ul className={"flex flex-col mt-5 lg:mt-0 space-y-2 lg:space-y-3"}>
                         {navItem?.map((item) => (
                             <li key={item.id}
-                                className={"text-xl hover:text-red-300 hover:scale-105 cursor-pointer transition-colors duration-300 "}>{item.name}</li>
+                                className={" lg:text-xl hover:text-red-300 hover:scale-105 cursor-pointer transition-colors duration-300 "}>{item.name}</li>
                         ))}
                     </ul>
                     <div>
-                        <p className={"font-Sora"}>Follow Us</p>
+                        <p className={"font-Sora mt-5 lg:mt-0"}>Follow Us</p>
                         <div className={"flex space-x-3 mt-4"}>
                             {socialIcons?.map((icon) => (
                                 <div key={icon.id}
--- src/Component/Subscribe.jsx
+++ src/Component/Subscribe.jsx
@@ -5,33 +5,35 @@ import Button from "../Common Component/Button.jsx";
 
 function Subscribe() {
     return (
-        <div
-            className={"container hover:scale-x-95 transition duration-200 mx-auto px-4 lg:px-0 bg-secondary py-10 lg:py-9 lg:mt-32 mt-20 rounded-2xl relative z-10"}>
-            <div className={"flex  justify-evenly  items-center lg:gap-x-10 gap-4"}>
-                <div><img src={dog4 || ""} alt="dog" className={"lg:w-[330px]"}/></div>
-                <div className={"space-y-5 lg:text-left text-center"}>
-                    <h2 className={"heading2"}>Get Pawsome News!</h2>
-                    <p>
-                        Exclusive training tips, ticks, product deals and more.
-                    </p>
-                    <InputCompo placeholder={"Enter email...."} type={"email"}
-                                icon={<svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg"
-                                           viewBox="0 0 24 24">
-                                    <g
-                                        strokeLinejoin="round"
-                                        strokeLinecap="round"
-                                        strokeWidth="2.5"
-                                        fill="none"
-                                        stroke="currentColor"
-                                    >
-                                        <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                                        <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                                    </g>
-                                </svg>}/>
-                    <Button btnText={"Subscribe"} styles={"mt-5"}/>
+        <div className={"mx-4"}>
+            <div
+                className={"container hover:scale-x-95 transition duration-200 mx-auto px-4 lg:px-0 bg-secondary py-10 lg:py-9 lg:mt-32 mt-20 rounded-2xl relative z-10"}>
+                <div className={"flex flex-col lg:flex-row justify-evenly  items-center lg:gap-x-10 gap-4"}>
+                    <div><img src={dog4 || ""} alt="dog" className={"lg:w-[330px] w-[200px]"}/></div>
+                    <div className={"space-y-5 lg:text-left text-center"}>
+                        <h2 className={"heading2"}>Get Pawsome News!</h2>
+                        <p>
+                            Exclusive training tips, ticks, product deals and more.
+                        </p>
+                        <InputCompo placeholder={"Enter email...."} type={"email"}
+                                    icon={<svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg"
+                                               viewBox="0 0 24 24">
+                                        <g
+                                            strokeLinejoin="round"
+                                            strokeLinecap="round"
+                                            strokeWidth="2.5"
+                                            fill="none"
+                                            stroke="currentColor"
+                                        >
+                                            <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                                            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                                        </g>
+                                    </svg>}/>
+                        <Button btnText={"Subscribe"} styles={"mt-2 lg:mt-5"}/>
+                    </div>
                 </div>
-            </div>
 
+            </div>
         </div>
     );
 }
